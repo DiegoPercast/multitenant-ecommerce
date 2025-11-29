062d7d63d520d05bc1dcf058e111df6c1f306f12
DIEGO PEREZ CASTILLO <2024030445@upsin.edu.mx>
2025-11-29 14:18:33 -0700

    10: category pages

--- src/modules/home/ui/components/search-filters/categories-sidebar.tsx
+++ src/modules/home/ui/components/search-filters/categories-sidebar.tsx
@@ -45,7 +45,7 @@ export const CategoriesSidebar = ({ open, onOpenChange}: Props) => {
 			// This is a leaf category (no subcategories)
 			if (parentCategories && selectedCategory) {
 				// This is a subcategory - navigate to /category/subcategory
-				router.push(`${selectedCategory.slug}/${category.slug}`)
+				router.push(`/${selectedCategory.slug}/${category.slug}`)
 			} else {
 				// This is a main category - navigate to /category
 				if (category.slug === "all") {
